86c335f7bb1330cb686da8668cfbd0e324b15a64
Ganesh Viswanathan <dev@genotrance.com>
2016-08-18 15:28:13 -0500

    Build fixes
    - Don't delete MSVC files from build
    - Only UPX Python binaries, not MS redistributable

--- build.bat
+++ build.bat
@@ -4,14 +4,12 @@ rmdir /s /q build
 rmdir /s /q __pycache__
 rmdir /s /q dist
 
-pyinstaller -w -i px.ico px.py
+pyinstaller --clean --noupx -w -i px.ico px.py
 copy px.ini dist\px\.
 copy README dist\px\README.txt
+upx --best dist\px\*.pyd
+upx --best dist\px\py*.dll
 
-del /q dist\px\msv*.dll
-del /q dist\px\api-*.dll
-del /q dist\px\ucrtbase.dll
-del /q dist\px\vcrun*.dll
 del /q px.spec
 rmdir /s /q build
 rmdir /s /q __pycache__
